e6284271e7667ea6372805a47af9a72253673ba1
ojas-17 <ojasbalsaraf@gmail.com>
2024-10-07 10:43:29 +0530

    changed x mark

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleCheck, faXmark } from '@fortawesome/free-solid-svg-icons';
 import { faCircle, faCircleXmark } from '@fortawesome/free-regular-svg-icons';
+import xMark from '../assets/x-mark.png'
 
 function TodoItem({todos, deleteTodo, toggleComplete}) {
 
@@ -28,16 +29,12 @@ function TodoItem({todos, deleteTodo, toggleComplete}) {
                     // onClick={() => toggleComplete(todo.id)}
                     />
                     
-                    <span
-                    className='cursor-pointer text-xl hover:bg-slate-400 flex justify-center items-center rounded-full'
-                    style={{
-                        lineHeight: '30px'
-                    }}
+                    <img
+                    className='h-5 rounded-full p-1 hover:bg-slate-400'
+                    src={xMark}
                     onClick={() => deleteTodo(todo.id)}
-                    >
-                        {/* {"\u00d7"} */}
-                        <FontAwesomeIcon icon={faCircleXmark} />
-                    </span>
+                    />
+
                 </div>
             )
         })}
